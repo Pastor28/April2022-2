43d77ad93d9dab01efc09b1847d5e9d1cd0a64de
Pastor28 <dgorz@interia.com>
2022-04-23 17:25:04 +0200

    first

---
+++ start.py
@@ -0,0 +1,19 @@
+print ('Hello world')
+# print('Druga instrukacja')
+x = 3  #integer
+f = 3.24  #float
+name = "Python" #string
+arr = ["apple", "banana", "cherry"]
+
+dict = {"name" : "John", "age" : 36}
+
+name = name[1:3]
+
+
+#x = 1/3
+sum = x + f
+
+# sumInteger = (int(x) + int(f))
+
+print(dict["age"])
+# print(sumInteger)
